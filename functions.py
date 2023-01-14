c0628c7aa5b09dcdd57b0d2a29f89f4c46a22727
neeischal <92285138+neeischal@users.noreply.github.com>
2023-01-14 13:21:42 +0545

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+#to find square root
+def square_root(num):
+
+    square_root=str(num **0.5)
+    square_root2='√'+square_root
+    return square_root2
+
+
+
+#prime or not
+def prime_number(a):
+    if a <=1:
+        return(f"{a} is not a prime number.")
+    else:
+        b=range(2,a)
+        for i in b:
+            if a % i==0:
+              return(f"{a} is not a prime number.")
+            break      
+        else:
+            return(f"{a} is a prime number.")
+
+def factorial(a):
+    if a == 0 :
+        return 1
+    return a* factorial(a-1)
+
